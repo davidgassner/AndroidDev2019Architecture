d4f45c06680d29cac5254572befa6b8e77421ac4
David Gassner <dgassnerdev@gmail.com>
2019-03-11 11:19:35 -0700

    02_01e

--- app/src/main/java/com/example/android/architecture/MainActivity.kt
+++ app/src/main/java/com/example/android/architecture/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.android.architecture
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val imageViews by lazy {
+        arrayOf<ImageView>(die1, die2, die3, die4, die5)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         lifecycle.addObserver(MyLifecycleObserver())
+
+        headline.text = "Welcome to my app"
+
+        for (imageView in imageViews) {
+            imageView.setImageResource(R.drawable.die_6)
+        }
     }
 
 }
