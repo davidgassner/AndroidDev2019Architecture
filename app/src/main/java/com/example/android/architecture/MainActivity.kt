36af19f32c4a22836d35eebbfab0f852cf7040a7
David Gassner <dgassnerdev@gmail.com>
2019-03-12 13:41:47 -0700

    02_08e

--- app/src/main/java/com/example/android/architecture/MainActivity.kt
+++ app/src/main/java/com/example/android/architecture/MainActivity.kt
@@ -3,19 +3,15 @@ package com.example.android.architecture
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.android.architecture.util.DiceHelper
 import com.example.android.architecture.viewModel.DiceViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel : DiceViewModel
-
-    private lateinit var dice: IntArray
-    private lateinit var headlineText: String
 
     private val imageViews by lazy {
         arrayOf<ImageView>(
@@ -37,26 +33,20 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProviders.of(this)
             .get(DiceViewModel::class.java)
-
-        headlineText = savedInstanceState?.getString(HEADLINE_TEXT)
-            ?: getString(R.string.welcome)
-        dice = savedInstanceState?.getIntArray(DICE_COLLECTION)
-            ?: intArrayOf(6, 6, 6, 6, 6)
+        viewModel.headline.observe(this, Observer {
+            headline.text = it
+        })
+        viewModel.dice.observe(this, Observer {
+            updateDisplay(it)
+        })
 
         lifecycle.addObserver(MyLifecycleObserver())
 
-        fab.setOnClickListener { fabClickHandler() }
+        fab.setOnClickListener { viewModel.rollDice() }
 
-        updateDisplay()
     }
 
-    private fun fabClickHandler() {
-        dice = DiceHelper.rollDice()
-        headlineText = DiceHelper.evaluateDice(this, dice)
-        updateDisplay()
-    }
-
-    private fun updateDisplay() {
+    private fun updateDisplay(dice: IntArray) {
         for (i in 0 until imageViews.size) {
             val drawableId = when (dice[i]) {
                 1 -> R.drawable.die_1
@@ -69,13 +59,6 @@ class MainActivity : AppCompatActivity() {
             }
             imageViews[i].setImageResource(drawableId)
         }
-        headline.text = headlineText
-    }
-
-    override fun onSaveInstanceState(outState: Bundle?) {
-        outState?.putString(HEADLINE_TEXT, headlineText)
-        outState?.putIntArray(DICE_COLLECTION, dice)
-        super.onSaveInstanceState(outState)
     }
 
 }
--- app/src/main/java/com/example/android/architecture/viewModel/DiceViewModel.kt
+++ app/src/main/java/com/example/android/architecture/viewModel/DiceViewModel.kt
@@ -3,11 +3,25 @@ package com.example.android.architecture.viewModel
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.example.android.architecture.LOG_TAG
+import com.example.android.architecture.R
+import com.example.android.architecture.util.DiceHelper
 
-class DiceViewModel(app: Application): AndroidViewModel(app) {
+class DiceViewModel(app: Application) : AndroidViewModel(app) {
+
+    val headline = MutableLiveData<String>()
+    val dice = MutableLiveData<IntArray>()
+    private val context = app
 
     init {
         Log.i(LOG_TAG, "View model created")
+        headline.value = context.getString(R.string.welcome)
+        dice.value = intArrayOf(6, 6, 6, 6, 6)
+    }
+
+    fun rollDice() {
+        dice.value = DiceHelper.rollDice()
+        headline.value = DiceHelper.evaluateDice(context, dice.value)
     }
 }
