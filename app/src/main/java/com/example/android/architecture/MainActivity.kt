90f0ecfecc6b3d1c26896b243957547bdc229996
David Gassner <dgassnerdev@gmail.com>
2019-03-12 12:09:49 -0700

    02_05e

--- app/src/main/java/com/example/android/architecture/MainActivity.kt
+++ app/src/main/java/com/example/android/architecture/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.android.architecture.util.DiceHelper
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -37,10 +38,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fabClickHandler() {
-        Toast.makeText(
-            this, "Replace with your own action",
-            Toast.LENGTH_LONG
-        ).show()
+        val dice = DiceHelper.rollDice()
+        updateDisplay(dice)
+    }
+
+    private fun updateDisplay(dice: IntArray) {
+        for (i in 0 until imageViews.size) {
+            val drawableId = when (dice[i]) {
+                1 -> R.drawable.die_1
+                2 -> R.drawable.die_2
+                3 -> R.drawable.die_3
+                4 -> R.drawable.die_4
+                5 -> R.drawable.die_5
+                6 -> R.drawable.die_6
+                else -> R.drawable.die_6
+            }
+            imageViews[i].setImageResource(drawableId)
+        }
+        headline.text = DiceHelper.evaluateDice(this, dice)
     }
 
 }
