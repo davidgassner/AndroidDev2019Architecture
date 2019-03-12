d289f4c2eaa320421dbe867de48e9c72ce4aecab
David Gassner <dgassnerdev@gmail.com>
2019-03-12 14:11:28 -0700

    03_01e

---
+++ app/src/main/java/com/example/android/architecture/DiceActivity.kt
@@ -0,0 +1,64 @@
+package com.example.android.architecture
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.android.architecture.viewModel.DiceViewModel
+import kotlinx.android.synthetic.main.activity_dice.*
+
+class DiceActivity : AppCompatActivity() {
+
+    private lateinit var viewModel : DiceViewModel
+
+    private val imageViews by lazy {
+        arrayOf<ImageView>(
+            findViewById(R.id.die1),
+            findViewById(R.id.die2),
+            findViewById(R.id.die3),
+            findViewById(R.id.die4),
+            findViewById(R.id.die5)
+        )
+    }
+    private val headline by lazy {
+        findViewById<TextView>(R.id.headline)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dice)
+        setSupportActionBar(toolbar)
+
+        viewModel = ViewModelProviders.of(this)
+            .get(DiceViewModel::class.java)
+        viewModel.headline.observe(this, Observer {
+            headline.text = it
+        })
+        viewModel.dice.observe(this, Observer {
+            updateDisplay(it)
+        })
+
+        lifecycle.addObserver(MyLifecycleObserver())
+
+        fab.setOnClickListener { viewModel.rollDice() }
+
+    }
+
+    private fun updateDisplay(dice: IntArray) {
+        for (i in 0 until imageViews.size) {
+            val drawableId = when (dice[i]) {
+                1 -> R.drawable.die_1
+                2 -> R.drawable.die_2
+                3 -> R.drawable.die_3
+                4 -> R.drawable.die_4
+                5 -> R.drawable.die_5
+                6 -> R.drawable.die_6
+                else -> R.drawable.die_6
+            }
+            imageViews[i].setImageResource(drawableId)
+        }
+    }
+
+}
