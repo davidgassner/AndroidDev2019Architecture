0a045ae7d5d243c938e6481828a34beefd31880e
David Gassner <dgassnerdev@gmail.com>
2019-03-12 12:59:06 -0700

    02_06e

--- app/src/main/java/com/example/android/architecture/MainActivity.kt
+++ app/src/main/java/com/example/android/architecture/MainActivity.kt
@@ -10,6 +10,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var dice: IntArray
+    private lateinit var headlineText: String
+
     private val imageViews by lazy {
         arrayOf<ImageView>(
             findViewById(R.id.die1),
@@ -28,21 +31,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        headlineText = savedInstanceState?.getString(HEADLINE_TEXT)
+            ?: getString(R.string.welcome)
+        dice = savedInstanceState?.getIntArray(DICE_COLLECTION)
+            ?: intArrayOf(6, 6, 6, 6, 6)
+
         lifecycle.addObserver(MyLifecycleObserver())
 
         fab.setOnClickListener { fabClickHandler() }
-        headline.text = getString(R.string.welcome)
-        for (imageView in imageViews) {
-            imageView.setImageResource(R.drawable.die_6)
-        }
+
+        updateDisplay()
     }
 
     private fun fabClickHandler() {
-        val dice = DiceHelper.rollDice()
-        updateDisplay(dice)
+        dice = DiceHelper.rollDice()
+        headlineText = DiceHelper.evaluateDice(this, dice)
+        updateDisplay()
     }
 
-    private fun updateDisplay(dice: IntArray) {
+    private fun updateDisplay() {
         for (i in 0 until imageViews.size) {
             val drawableId = when (dice[i]) {
                 1 -> R.drawable.die_1
@@ -55,7 +62,13 @@ class MainActivity : AppCompatActivity() {
             }
             imageViews[i].setImageResource(drawableId)
         }
-        headline.text = DiceHelper.evaluateDice(this, dice)
+        headline.text = headlineText
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putString(HEADLINE_TEXT, headlineText)
+        outState?.putIntArray(DICE_COLLECTION, dice)
+        super.onSaveInstanceState(outState)
     }
 
 }
