80871d8e25fffb45d4fda012820610c64655aed7
David Gassner <dgassnerdev@gmail.com>
2019-03-13 13:53:32 -0700

    04_01 Added new About activity and lorem ipsum string resource

---
+++ app/src/main/java/com/example/android/architecture/AboutActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android.architecture
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
