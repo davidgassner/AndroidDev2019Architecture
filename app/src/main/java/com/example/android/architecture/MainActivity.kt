f048894aea02cbb37d1a1c0dadb50d60e8dc29df
David Gassner <dgassnerdev@gmail.com>
2019-03-14 10:28:19 -0700

    04_02e

--- app/src/main/java/com/example/android/architecture/MainActivity.kt
+++ app/src/main/java/com/example/android/architecture/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_nav)
 
         rollButton.setOnClickListener{
             val intent = Intent(this, DiceActivity::class.java)
