645f0fdbb61a33ec59259d6bb7b1f5df0190aaf0
David Gassner <dgassnerdev@gmail.com>
2019-03-14 12:31:02 -0700

    04_05e

--- app/src/main/java/com/example/android/architecture/MainActivity.kt
+++ app/src/main/java/com/example/android/architecture/MainActivity.kt
@@ -5,18 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener {
+    private val drawerLayout by lazy {
+        findViewById<DrawerLayout>(R.id.drawer_layout)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_nav)
 
-        rollButton.setOnClickListener{
+        rollButton.setOnClickListener {
             val intent = Intent(this, DiceActivity::class.java)
             startActivity(intent)
         }
+
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        navView.setNavigationItemSelectedListener(this)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,4 +45,14 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        drawerLayout.closeDrawer(GravityCompat.START)
+        when (item.itemId) {
+            R.id.action_about -> AboutActivity.start(this)
+        }
+        return true
+    }
+
+
 }
