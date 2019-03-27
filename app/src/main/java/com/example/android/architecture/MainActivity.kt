79bf2d8ccdd77533dd6280616ff13e861c51cc43
David Gassner <david.gassner@gmail.com>
2019-03-27 14:23:43 -0700

    Moved soundpool management to view model Removed data binding

--- app/src/main/java/com/example/android/architecture/MainActivity.kt
+++ app/src/main/java/com/example/android/architecture/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.android.architecture
 
-import android.media.SoundPool
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,20 +8,11 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.android.architecture.viewModel.DiceViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import android.media.AudioManager
-import android.os.Build
-import android.util.Log
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: DiceViewModel
-
-    private lateinit var soundPool: SoundPool
-    private var soundId: Int = 0
-    private var soundLoaded = false
-
-    val imageViews by lazy {
+    private val imageViews by lazy {
         arrayOf<ImageView>(die1, die2, die3, die4, die5)
     }
 
@@ -31,40 +21,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        viewModel = ViewModelProviders.of(this).get(DiceViewModel::class.java)
-        viewModel.headline.observe(this, Observer {
-            headline.text = it
-        })
-        viewModel.dice.observe(this, Observer {
-            updateDisplay(it)
-        })
+        initViewModel()
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            soundPool = SoundPool.Builder()
-                .setMaxStreams(5)
-                .build()
-        } else {
-            @Suppress("DEPRECATION")
-            soundPool = SoundPool(5, AudioManager.STREAM_MUSIC, 0)
-        }
-        soundPool.setOnLoadCompleteListener { _, _, status ->
-            soundLoaded = (status == 0)
-        }
-        soundId = soundPool.load(this, R.raw.roll_dice, 1)
-
-        fab.setOnClickListener {
-            viewModel.rollDice()
-            if (soundLoaded) {
-                soundPool.play(soundId, 1f, 1f, 0, 0, 1f)
-            }
-        }
+        fab.setOnClickListener { viewModel.rollDice() }
     }
 
+    /**
+     * Initialize the viewModel
+     */
+    private fun initViewModel() {
+        viewModel = ViewModelProviders
+            .of(this)
+            .get(DiceViewModel::class.java)
+        viewModel.headline.observe(this, Observer { headline.text = it })
+        viewModel.dice.observe(this, Observer { updateDisplay(it) })
+    }
+
+    /**
+     * Update the display of dice
+     */
     private fun updateDisplay(dice: IntArray?) {
         for (i in 0 until imageViews.size) {
-            val drawableId = resources.getIdentifier(
-                "die_${dice?.get(i)}", "drawable", packageName
-            )
+            val drawableId = when (dice?.get(i)) {
+                1 -> R.drawable.die_1
+                2 -> R.drawable.die_2
+                3 -> R.drawable.die_3
+                4 -> R.drawable.die_4
+                5 -> R.drawable.die_5
+                6 -> R.drawable.die_6
+                else -> R.drawable.die_6
+            }
             imageViews[i].setImageResource(drawableId)
         }
     }
--- app/src/main/java/com/example/android/architecture/viewModel/DiceViewModel.kt
+++ app/src/main/java/com/example/android/architecture/viewModel/DiceViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.android.architecture.viewModel
 
 import android.app.Application
+import android.content.Context
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.example.android.architecture.R
@@ -10,9 +14,20 @@ class DiceViewModel(app: Application) : AndroidViewModel(app) {
     val headline = MutableLiveData<String>()
     val dice = MutableLiveData<IntArray?>()
 
+    private lateinit var soundPool: SoundPool
+
+    private var soundId = 0
+    private var soundLoaded = false
+    private var context: Context
+
     init {
+//      Initialize properties
         headline.value = getApplication<Application>().getString(R.string.roll_em)
-        dice.value = intArrayOf(6, 6, 6, 6, 6)
+        dice.value = intArrayOf(5, 4, 3, 2, 1)
+        context = app
+
+//      Set up sound pool
+        initSoundPool()
     }
 
     fun rollDice() {
@@ -20,5 +35,23 @@ class DiceViewModel(app: Application) : AndroidViewModel(app) {
         headline.value = DiceHelper.evaluateDice(
             getApplication(), dice.value
         )
+        if (soundLoaded) {
+            soundPool.play(soundId, 1f, 1f, 0, 0, 1f)
+        }
+    }
+
+    private fun initSoundPool() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            soundPool = SoundPool.Builder()
+                .setMaxStreams(5)
+                .build()
+        } else {
+            @Suppress("DEPRECATION")
+            soundPool = SoundPool(5, AudioManager.STREAM_MUSIC, 0)
+        }
+        soundPool.setOnLoadCompleteListener { _, _, status ->
+            soundLoaded = (status == 0)
+        }
+        soundId = soundPool.load(context, R.raw.roll_dice, 1)
     }
 }
